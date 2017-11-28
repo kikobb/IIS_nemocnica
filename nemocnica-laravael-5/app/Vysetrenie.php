86ba3a2df855ea47038601ae57cc87880cb7b692
kiko <xbarna02@stud.vutbr.cz>
2017-11-28 22:49:18 +0100

    vysetrenie pridane gety

--- nemocnica-laravael-5/app/Vysetrenie.php
+++ nemocnica-laravael-5/app/Vysetrenie.php
@@ -9,6 +9,18 @@ class Vysetrenie extends Model
     //definovanie tabulky spojenej s modelom (implicitne k nazvu sa prida s a vyhlada tabulka)
     protected $table = 'vysetrenia';
 
+    public function getDoktor(){
+        $this->doktor()->first();
+    }
+
+    public function getPacient(){
+        $this->pacient()->first();
+    }
+
+    public function getPodaneLiekyArr(){
+        $this->podane_lieky()->get()->toArray();
+    }
+
     public function doktor(){
         return $this->belongsTo(User::class, 'doktor_id', 'id');
     }
