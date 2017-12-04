957f913c8806e3fa3ba2c9f61b88339418e15dc9
lucka <xtusim00@stud.fit.vutbr.cz>
2017-12-04 21:14:00 +0100

    updave selectov vo vyhladavani

--- nemocnica-laravael-5/resources/views/pacient/confirm.blade.php
+++ nemocnica-laravael-5/resources/views/pacient/confirm.blade.php
@@ -6,47 +6,48 @@
         <h2> Údaje boli uložené</h2>
 
 
-        <div>
+        <div class="row">
             <div class="col-md-5">Meno:</div>
             <div class="col-md-6"> {{$osoba->meno}}</div>
         </div>
 
 
-        <div>
+        <div class="row">
             <div class="col-md-5">Priezvisko:</div>
             <div class="col-md-6"> {{$osoba->priezvisko}}</div>
         </div>
 
-        <div>
+        <div class="row">
             <div class="col-md-5">Rodné číslo:</div>
             <div class="col-md-6"> {{$osoba->rodne_cislo}}</div>
         </div>
 
-        <div>
+        <div class="row">
             <div class="col-md-5">Číslo poisťovne:</div>
             <div class="col-md-6"> {{$osoba->cislo_poistovne}}</div>
         </div>
 
-        <div>
+        <div class="row">.
             <div class="col-md-5">Mesto:</div>
             <div class="col-md-6"> {{$osoba->mesto}}</div>
         </div>
 
-        <div>
+        <div class="row">
             <div class="col-md-5">PSČ:</div>
             <div class="col-md-6">{{$osoba->psc}} </div>
         </div>
 
-        <div>
+        <div class="row">
             <div class="col-md-5">Ulica a číslo domu:</div>
             <div class="col-md-6">{{$osoba->ulica_cislo}} </div>
         </div>
-        <div>
+
+        <div class="row">
             <div class="col-md-5">Štát:</div>
             <div class="col-md-6">{{$osoba->stat}} </div>
         </div>
 
-        <div>
+        <div class="row">
             <div class="col-md-5">Dátum narodenia:</div>
             <div class="col-md-6">{{$osoba->datum_narodenia}} </div>
         </div>
@@ -56,11 +57,11 @@
     @if($currUser->pozicia == 'doktor' || $currUser->pozicia == 'sestra' || $currUser->pozicia == 'prijemca')
         <div class="col-sm-6 " id="confirm">
 
-            <a href="/zamestnanec/create" class="btn btn-info btn-lg">
+            <a href="{{ route('pacient.create') }}" class="btn btn-info btn-lg">
                 Ďalšia registrácia
             </a>
 
-            <a href="{{ route('zamestnanec.edit', $osoba->id) }}" class="btn btn-info btn-lg">
+            <a href="{{ route('pacient.edit', $osoba->id) }}" class="btn btn-info btn-lg">
                 Zmena údajov
             </a>
 
