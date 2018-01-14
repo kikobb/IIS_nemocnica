91db920854fc6da8cb3d8578de448d46d3fa6b8d
k <xbarna02@stud.fit.vutbr.cz>
2018-01-14 22:44:51 +0100

    confirm fix

--- nemocnica-laravael-5/resources/views/podany_liek/confirm.blade.php
+++ nemocnica-laravael-5/resources/views/podany_liek/confirm.blade.php
@@ -5,19 +5,20 @@
     <div class="col-sm-6 ">
         <h2> Údaje boli uložené</h2>
 
+
         <div class="row">
             <div class="col-md-5">Názov lieku:</div>
-            <div class="col-md-6"> {{$podany_liek->nazov}}</div>
+            <div class="col-md-6"> {{$podany_liek->getNazov()}}</div>
         </div>
 
         <div class="row">
             <div class="col-md-5">Čas podávania:</div>
-            <div class="col-md-6">{{$podany_liek->ucinna_latka}} </div>
+            <div class="col-md-6">{{$podany_liek->getCas()}} </div>
         </div>
 
         <div class="row">
             <div class="col-md-5">Kontraindikácia:</div>
-            <div class="col-md-6">{{$podany_liek->kontraindikacia}}</div>
+            <div class="col-md-6">{{$podany_liek->getMnozstvo()}}</div>
         </div>
 
 
@@ -25,11 +26,11 @@
 
     <div class="col-sm-6 " id="confirm">
 
-        <a href="{{ route('podany_liek.create', $podany_liek->id) }}" class="btn btn-info btn-lg">
+        <a href="{{ route('create_podany_liek', $podany_liek->id) }}" class="btn btn-info btn-lg">
             Ďalšia registrácia
         </a>
 
-        <a href="{{ route('podany_liek.edit', $podany_liek->id) }}" class="btn btn-info btn-lg">
+        <a href="{{ route('podanyLiek.edit', $podany_liek->id) }}" class="btn btn-info btn-lg">
             Zmena údajov
         </a>
 
